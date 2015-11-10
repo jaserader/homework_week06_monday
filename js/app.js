03f373c3c31e92d0b708ec1e4d25ec95a397d36e
Jase Rader <jaserader@me.com>
2015-11-09 21:30:21 -0600

    first two exercises completed

---
+++ js/app.js
@@ -0,0 +1,107 @@
+var Blog = Backbone.Model.extend({
+  url: "http://tiny-starburst.herokuapp.com/collections/posts"
+});
+
+var Blogs = Backbone.Collection.extend({
+  url: "http://tiny-starburst.herokuapp.com/collections/posts"
+});
+
+var Person = Backbone.Model.extend({
+  url: "http://tiny-starburst.herokuapp.com/collections/people"
+});
+
+var Peoples = Backbone.Collection.extend({
+  url: "http://tiny-starburst.herokuapp.com/collections/people"
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+var BlogView = Backbone.View.extend({
+  tagname: "section",
+  template: _.template($('#blogInputTemplate').html()),
+
+  events: {
+    'click #blogSubmitBtn': 'handlerSubmitBtn'
+  },
+
+  send: function(){
+    var submit = this.$('#blogSubmitBtn').val();
+      console.log('click');
+
+    var title = this.$("#blogTitle").val();
+    var blogBody = this.$('#blogContent').val();
+
+    var newBlog = new Blog({
+       title: title,
+       body: blogBody
+    })
+      newBlog.save();
+  },
+
+});
+
+var ContactView = Backbone.View.extend({
+  tagname: "form",
+  template: _.template($('#peopleTemplate').html()),
+
+  events: {
+    'click #contactSubmitBtn': 'handlerSubmitBtn'
+  },
+
+  send: function(){
+    var submit = this.$('#blogSubmitBtn').val();
+      console.log('click');
+
+    var firstName = this.$("#firstName").val();
+    var lastName = this.$('#lastName').val();
+    var address = this.$("#address").val();
+    var phoneNumber = this.$('#phoneNumber').val();
+
+    var newPerson = new Person({
+       firstName: firstName,
+       lastName: lastName,
+       address: address,
+       phoneNumber: phoneNumber,
+
+    })
+      newPerson.save();
+  },
+
+
+  render: function(){
+    this.$el.html(this.template());
+  },
+
+  handlerSubmitBtn: function(event){
+    event.preventDefault;
+    this.send();
+    // console.log(body);
+  }
+
+});
+
+var BlogRouter = Backbone.Router.extend({
+
+  routes: {
+    "": "blogEntry",
+    "page2": "personEntry"
+  },
+
+  blogEntry: function (){
+    var view = new BlogView();
+    view.render();
+    $('#mainArea').html(view.$el);
+  },
+
+  personEntry: function (){
+    var views = new ContactView();
+    views.render();
+    $('#mainArea').html(views.$el);
+  }
+
+});
+
+var router = new BlogRouter();
+Backbone.history.start();
+
+// var blogView = new BlogView();
